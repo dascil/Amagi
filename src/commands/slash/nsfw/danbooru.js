1ea7f2cf9c9464ff04d10a7e541f676582bbfa34
Zach Dascil <dascilz@cs.washington.edu>
2023-04-07 17:20:45 -0700

    0.0.1.2v Bug Fixes
    - Bugs were fixed in the Danbooru command
      - More concise error messages
        - Server Error
        - Bad Tag Error
        - No Image Error
      - Command now retries if No Image Error occurs a set amount of times

--- src/commands/slash/nsfw/danbooru.js
+++ src/commands/slash/nsfw/danbooru.js
@@ -4,38 +4,97 @@ module.exports = {
   data: new SlashCommandBuilder()
     .setName("danbooru")
     .setDescription(
-      "Gives random danbooru image of specified tag. Use underscore for spaces. Default tag is azur_lane"
+      "Gives random danbooru image of specified tag. Default tag is azur_lane"
     )
     .addStringOption((option) =>
-      option.setName("tag").setDescription("Tag to look up").setRequired(false)
+      option
+        .setName("tag")
+        .setDescription("Tag of desired photo")
+        .setRequired(false)
     ),
   async execute(interaction, client) {
     const msg = await interaction.deferReply({
       fetchReply: false,
     });
 
+    const RETRIES = 3;
+    const NOT_FOUND_PROMISE = "That record was not found.";
+    // Expected errors that can occur
+    const SITE_UNREACHABLE_MSG =
+      "Unable to reach Danbooru.\nPlease check if Danbooru is going through maintanence or is down.";
+    const BAD_TAG_MSG =
+      "Tag does not exist on danbooru.\n Please make sure your spelling is correct.";
+    const NO_SUITABLE_PHOTO_MSG =
+      "Unable to find a suitable photo with that tag.\nPlease try again.";
+    // A new error has occured and needs to be investigated
+    let newMsg =
+      "An error has occured.\nPlease contact an admin and describe what happened.";
+
     try {
+      // Get tag or use default
       const tag = interaction.options.getString("tag") ?? "azur_lane";
       let url = "https://danbooru.donmai.us/posts/random.json?tags=" + tag;
-      let jsonOBJ = await fetch(url);
-      if (!jsonOBJ.ok) {
-        throw new Error(await jsonOBJ.text());
+      let jsonObj = null;
+      // If fetch requests fails due to bad image,
+      // it will retry a set amount of times before
+      // giving up and sending an error
+      for (let i = 0; i < RETRIES; i++) {
+        // Fetch request Danbooru API
+        jsonObj = await fetch(url);
+        // If error during fetch request
+        if (!jsonObj.ok) {
+          newMsg = SITE_UNREACHABLE_MSG;
+          let error = await jsonObj.json();
+          // Bad tag error
+          if (
+            error.hasOwnProperty("message") &&
+            error.message === NOT_FOUND_PROMISE
+          ) {
+            newMsg = BAD_TAG_MSG;
+            throw new Error(error.message);
+            // Danbooru site error
+          } else {
+            throw new Error(await jsonObj.text());
+          }
+        }
+        jsonObj = await jsonObj.json();
+        // Valid photo found with correct format
+        if (
+          jsonObj.hasOwnProperty("file_url") &&
+          (jsonObj.file_url.endsWith(".png") ||
+            json.file_url.endsWith(".jpg") ||
+            json.file_url.endsWith(".webp"))
+        ) {
+          break;
+        }
       }
-      jsonOBJ = await jsonOBJ.json();
-
-      if (!jsonOBJ.hasOwnProperty("file_url")) {
-        throw new Error("Returned JSON object does not contain a file.");
+      // Null edge case catch
+      if (jsonObj === null) {
+        console.trace();
+        throw new Error("jsonObj is null.");
       }
-      const newMsg = jsonOBJ.file_url;
-      await interaction.editReply({
-        content: newMsg,
-      });
-    } catch (err) {
+      // Still cannot find a suitable photo after retries
+      if (
+        !jsonObj.hasOwnProperty("file_url") || (
+        !jsonObj.file_url.endsWith(".jpg") &&
+        !jsonObj.file_url.endsWith(".png") &&
+        !jsonObj.file_url.endsWith(".webm"))
+      ) {
+        newMsg = NO_SUITABLE_PHOTO_MSG;
+        throw new Error(await jsonObj.text());
+      }
+      // Send picture
+      newMsg = jsonObj.file_url;
+      // Logs any error that occured
+    } catch (error) {
       const time = new Date().toLocaleTimeString();
-      console.log("\n")
-      console.log("TIME OF ERROR: " + time);
-      console.log(err);
-      const newMsg = `Something went wrong :( \n Please make sure that your spelling is correct.\n Your tag might also require the series (i.e. fischl_(genshin_impact)) \n If this problem persists, please contact an admin.`;
+      console.error("\nTIME OF ERROR: " + time);
+      console.error("/Danbooru command");
+      console.error(error);
+
+
+      // Sends reply to user
+    } finally {
       await interaction.editReply({
         content: newMsg,
       });
