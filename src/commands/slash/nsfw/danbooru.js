4f314e180cb78ece18f6ed4891ee6e85f6747964
Zach Dascil <dascilz@cs.washington.edu>
2023-04-08 22:32:44 -0700

    v0.0.1.3 Danbooru update
    - Danbooru command only works in nsfw channel
    - Code injection filter added

--- src/commands/slash/nsfw/danbooru.js
+++ src/commands/slash/nsfw/danbooru.js
@@ -4,7 +4,7 @@ module.exports = {
   data: new SlashCommandBuilder()
     .setName("danbooru")
     .setDescription(
-      "Gives random danbooru image of specified tag. Default tag is azur_lane"
+      "Return random image based on tags. Separate tags by space. Limit 2."
     )
     .addStringOption((option) =>
       option
@@ -18,6 +18,8 @@ module.exports = {
     });
 
     const RETRIES = 3;
+    const NOT_IN_A_NSFW_CHANNEL =
+      "This command can only be used in a NSFW channel.";
     const NOT_FOUND_PROMISE = "That record was not found.";
     // Expected errors that can occur
     const SITE_UNREACHABLE_MSG =
@@ -31,68 +33,72 @@ module.exports = {
       "An error has occured.\nPlease contact an admin and describe what happened.";
 
     try {
-      // Get tag or use default
-      const tag = interaction.options.getString("tag") ?? "azur_lane";
-      let url = "https://danbooru.donmai.us/posts/random.json?tags=" + tag;
-      let jsonObj = null;
-      // If fetch requests fails due to bad image,
-      // it will retry a set amount of times before
-      // giving up and sending an error
-      for (let i = 0; i < RETRIES; i++) {
-        // Fetch request Danbooru API
-        jsonObj = await fetch(url);
-        // If error during fetch request
-        if (!jsonObj.ok) {
-          newMsg = SITE_UNREACHABLE_MSG;
-          let error = await jsonObj.json();
-          // Bad tag error
+      if (!interaction.channel.nsfw) {
+        newMsg = NOT_IN_A_NSFW_CHANNEL;
+      } else {
+        // Get tag or use default
+        let tag = interaction.options.getString("tag") ?? "azur_lane";
+        const filter = /[{}\[\]<>/\\+*!?$&*]/g;
+        tag = tag.replace(filter, "");
+        let url = "https://danbooru.donmai.us/posts/random.json?tags=" + tag;
+        let jsonObj = null;
+        // If fetch requests fails due to bad image,
+        // it will retry a set amount of times before
+        // giving up and sending an error
+        for (let i = 0; i < RETRIES; i++) {
+          // Fetch request Danbooru API
+          jsonObj = await fetch(url);
+          // If error during fetch request
+          if (!jsonObj.ok) {
+            newMsg = SITE_UNREACHABLE_MSG;
+            let error = await jsonObj.json();
+            // Bad tag error
+            if (
+              error.hasOwnProperty("message") &&
+              error.message === NOT_FOUND_PROMISE
+            ) {
+              newMsg = BAD_TAG_MSG;
+              throw new Error(error.message);
+              // Danbooru site error
+            } else {
+              throw new Error(await jsonObj.text());
+            }
+          }
+          jsonObj = await jsonObj.json();
+          // Valid photo found with correct format
           if (
-            error.hasOwnProperty("message") &&
-            error.message === NOT_FOUND_PROMISE
+            jsonObj.hasOwnProperty("file_url") &&
+            (jsonObj.file_url.endsWith(".png") ||
+              jsonObj.file_url.endsWith(".jpg") ||
+              jsonObj.file_url.endsWith(".webp"))
           ) {
-            newMsg = BAD_TAG_MSG;
-            throw new Error(error.message);
-            // Danbooru site error
-          } else {
-            throw new Error(await jsonObj.text());
+            break;
           }
         }
-        jsonObj = await jsonObj.json();
-        // Valid photo found with correct format
-        if (
-          jsonObj.hasOwnProperty("file_url") &&
-          (jsonObj.file_url.endsWith(".png") ||
-            jsonObj.file_url.endsWith(".jpg") ||
-            jsonObj.file_url.endsWith(".webp"))
-        ) {
-          break;
+        // Null edge case catch
+        if (jsonObj === null) {
+          console.trace();
+          throw new Error("jsonObj is null.");
         }
+        // Still cannot find a suitable photo after retries
+        if (
+          !jsonObj.hasOwnProperty("file_url") ||
+          (!jsonObj.file_url.endsWith(".jpg") &&
+            !jsonObj.file_url.endsWith(".png") &&
+            !jsonObj.file_url.endsWith(".webm"))
+        ) {
+          newMsg = NO_SUITABLE_PHOTO_MSG;
+          throw new Error(await jsonObj.text());
+        }
+        // Send picture
+        newMsg = jsonObj.file_url;
+        // Logs any error that occured
       }
-      // Null edge case catch
-      if (jsonObj === null) {
-        console.trace();
-        throw new Error("jsonObj is null.");
-      }
-      // Still cannot find a suitable photo after retries
-      if (
-        !jsonObj.hasOwnProperty("file_url") || (
-        !jsonObj.file_url.endsWith(".jpg") &&
-        !jsonObj.file_url.endsWith(".png") &&
-        !jsonObj.file_url.endsWith(".webm"))
-      ) {
-        newMsg = NO_SUITABLE_PHOTO_MSG;
-        throw new Error(await jsonObj.text());
-      }
-      // Send picture
-      newMsg = jsonObj.file_url;
-      // Logs any error that occured
     } catch (error) {
       const time = new Date().toLocaleTimeString();
       console.error("\nTIME OF ERROR: " + time);
       console.error("/Danbooru command");
       console.error(error);
-
-
       // Sends reply to user
     } finally {
       await interaction.editReply({
