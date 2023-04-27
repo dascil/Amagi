6dcd8ebfe7402a36b4c716088a5b4a4eaa3f618f
Zach Dascil <dascilz@cs.washington.edu>
2023-04-27 12:48:28 -0700

    Condensed sauce command objects
    - Combined Danbooru, Yandere, and Gelbooru into FetchImage class
      - FetchImage class is initialized with client, and is referenced

---
+++ src/instances/objects/slash/fetch/ImageBoards.ts
@@ -0,0 +1,38 @@
+import Board from "../../../interfaces/slash/fetch/BoardInterface"
+
+export const DANBOORU_BOARD: Board = {
+    name: "danbooru",
+    image_query: "https://danbooru.donmai.us/posts/random.json?tags=",
+    tag_query: "https://danbooru.donmai.us/tags.json?search[order]=count&search[name_matches]=",
+    tag_query_wildcard: "*",
+    base_tag: "",
+    sfw_base_tag: "rating:g",
+    sfw_rating: "g",
+    nsfw_ratings: ["s","q","e"]
+}
+
+export const GELBOORU_BOARD: Board = {
+    name: "gelbooru",
+    image_query: "https://gelbooru.com/index.php?page=dapi&q=index&json=1&limit=1&s=post&tags=sort:random+",
+    tag_query: "https://gelbooru.com/index.php?page=dapi&s=tag&q=index&json=1&limit=30&orderby=count&name_pattern=",
+    tag_query_wildcard: "%",
+    base_tag: "-rating:explicit",
+    sfw_base_tag: "rating:general",
+    sfw_rating: "general",
+    nsfw_ratings: ["questionable","suggestive","explicit"]
+}
+
+export const YANDERE_BOARD: Board = {
+    name: "yandere",
+    image_query: "https://yande.re/post.json?limit=1&tags=order:random+",
+    tag_query: "https://yande.re/tag.json?limit=30&type=&order=count&name=",
+    tag_query_wildcard: "*",
+    base_tag: "-rating:e",
+    sfw_base_tag: "rating:s",
+    sfw_rating: "g",
+    nsfw_ratings: ["q","e"]
+}
+
+export default {
+    DANBOORU:DANBOORU_BOARD, GELBOORU:GELBOORU_BOARD, YANDERE:YANDERE_BOARD
+}
