9d10929bff868fe5513119af34576b73cf7acda3
dascil <dascilz@cs.washington.edu>
2023-04-30 11:04:30 -0700

    Finished podium slash command and added lr slash command

--- src/commands/slash/game/lr.ts
+++ src/commands/slash/game/lr.ts
@@ -1,19 +1,15 @@
 import { ChatInputCommandInteraction, SlashCommandBuilder } from "discord.js";
 import AmagiClient from "../../../instances/classes/client/AmagiClient";
-const react1 = process.env['123']!;
-const react2 = process.env['132']!;
-const react3 = process.env['213']!;
-const react4 = process.env['231']!;
-const react5 = process.env['312']!;
-const react6 = process.env['321']!;
+const react1 = process.env['L']!;
+const react2 = process.env['R']!;
 
 module.exports = {
     data: new SlashCommandBuilder()
-        .setName('podium')
-        .setDescription('Rate the characters in the photo 1 (highest) to 3 (lowest)')
+        .setName('lr')
+        .setDescription('Pick your favorite left (L) or right (R)')
         .addSubcommand(command => command
             .setName("link")
-            .setDescription("Podium game using photo link")
+            .setDescription("LR game using photo link")
             .addStringOption(option => option
                 .setName("link")
                 .setDescription("Link to photo")
@@ -22,20 +18,20 @@ module.exports = {
         )
         .addSubcommand(command => command
             .setName("attachment")
-            .setDescription("Podium game using attached photo")
+            .setDescription("LR game using attached photo")
             .addAttachmentOption(option => option
                 .setName("attachment")
                 .setDescription("Photo to be attached")
                 .setRequired(true)
             )
         ),
-    usage: "podium {subcommand}",
-    return: "Below is a list of the podium commands: \n\n \
+    usage: "lr {subcommand}",
+    return: "Below is a list of the lr commands: \n\n \
              `link`\n \
-             Reacts with podium emotes to linked photo\n \
+             Reacts with lr emotes to linked photo\n \
              **Parameters:** `link` **Required:** True \n \
              `attachment`\n \
-             Reacts with podium emotes to attached photo\n \
+             Reacts with lr emotes to attached photo\n \
              **Parameters:** `attachment` **Required:** True \n",
     async execute(interaction: ChatInputCommandInteraction, client: AmagiClient) {
         await interaction.deferReply();
@@ -45,10 +41,10 @@ module.exports = {
         let photoGood = true;
 
         if (subcommand === "link") {
-            // Checks if valid URL to an image is passed
             const link = interaction.options.getString("link")!;
+            // Verifies string link passed is a valid URL to an image
             try {
-                let url = new URL(link);
+                new URL(link);
             } catch (error) {
                 photoGood = false;
             }
@@ -64,31 +60,30 @@ module.exports = {
                 photoGood = false;
             }
         } else {
-            // Checks if an image with valid dimensions is passed
-            const attachment = interaction.options.getAttachment("attachment");
-            const link = attachment?.url;
+            // Check if the attachments are images with readable dimensions
+            const attachment = interaction.options.getAttachment("attachment")!;
+            const link = attachment.url ?? "";
+            if (attachment.contentType?.startsWith("image") && attachment.height !== null && attachment.width !== null) {
 
-            if (attachment?.contentType?.startsWith("image") && attachment.height !== null && attachment.width !== null) {
                 newMsg = link;
+
             } else {
                 newMsg = "Attachment is not valid. \nMake sure sure the image uploaded is a jpg, jpeg, png, or webp."
                 photoGood = false;
             }
+
         }
 
+        // Reacts with LR emotes
         await interaction.editReply({
             content: newMsg
         }).then(message => {
             if (photoGood) {
                 message.react(react1);
                 message.react(react2);
-                message.react(react3);
-                message.react(react4);
-                message.react(react5);
-                message.react(react6);
             }
         }).catch(error => {
-            console.log(client.failure("ERROR: Problem adding reactions to podium command"));
+            console.log(client.failure("ERROR: Problem adding reactions to lr command"));
             console.error(error);
         });
     },
